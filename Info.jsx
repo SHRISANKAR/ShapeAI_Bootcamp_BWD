8cb70712b88f2cfe9faf653515f179188e125f5a
SHRISANKAR <85781471+SHRISANKAR@users.noreply.github.com>
2021-06-26 17:39:08 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>Basic web dev react js camp</p>
+    </div>
+  );
+}
+
+export default Info;
